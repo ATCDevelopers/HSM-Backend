4738dd2a11a01c44117d5bf7960a6a249290112a
codakadaniel33 <kalimangasicostantino33@gmail.com>
2026-09-03 14:15:57 +0300

    fixing the .env loading

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit"
 import dotenv from 'dotenv'
 
 
-dotenv.config();
+dotenv.config({ path: './.env' });
 
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
